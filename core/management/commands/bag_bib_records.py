3993285570397686428ad35bf325232d252e4d46
Jackie Kazil <jkazil@loc.gov>
2013-02-04 17:05:31 -0500

    Adding the beginnings of a bag_in_place api call.

--- core/management/commands/bag_bib_records.py
+++ core/management/commands/bag_bib_records.py
@@ -1,6 +1,13 @@
 import logging
+import os
+import subprocess
 
+from datetime import date, datetime
 from optparse import make_option
+
+from cts.inventory import Bag
+from fabric.api import (env, run, settings, sudo,)
+from fabric.contrib.files import exists
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
@@ -9,6 +16,40 @@ from chronam.core.management.commands import configure_logging
 
 configure_logging('bag_bib_records.config', 'bag_bib_records.log')
 _logger = logging.getLogger(__name__)
+
+env.host_string = 'localhost'
+
+def copy_dir(data=settings.BIB_STORAGE):
+    # cp bib dir to new directory w/ the date attached.
+    bag_date = date.isoformat(date.today()).replace('-','')
+    abs_location = run('file -b %s' % data)
+    #abs_location = "symbolic link to `/vol/ndnp/chronam/bib-dev'"
+    abs_location = abs_location.strip("'").split("`")[1]
+    destination = '-'.join((abs_location, bag_date))
+
+    if not exists(destination):
+       
+        sudo('mkdir %s' % destination)
+        sudo('cp -r %s/. %s' % (data, destination))
+        # TODO: Add check to make sure file sizes are the same
+        # & everything transferred correctly.
+        # du --max-depth=0 %s % data
+    else:
+        return None
+
+    return destination
+
+def bag_in_place(directory):
+    client = get_client()
+    client.add_credentials(name=settings.CTS_EDEPOSIT_APPLICATION_USER,
+                           password=settings.CTS_EDEPOSIT_APPLICATION_PASSWORD)
+    request_params = {'bagInstanceKey': datetime.isoformat(),
+                    # TODO: Figure out what path variable is & set it.
+                        }
+    request_params = urllib.urlencode(request_params)
+    request_url = '%sservice_requests/bag_in_place' 
+    return
+
 
 class Command(BaseCommand):
     data = make_option('--data', 
@@ -21,11 +62,14 @@ class Command(BaseCommand):
     help = ''
 
     def handle(self, *args, **options):
-        data = options['data']
+        #data = options['data']
+    
+        # Make copy of bib directory
+        destination = copy_dir()
 
-        # cp bib append date to bib
-            # mkdir bib-$date (date should be 20130128 format)
-            # cp -r bib bib-$date/data
+        # Make bag in place
+        if destination:
+            bag_in_place(destination)        
         
         # hit cts to bag & receive content
             # create a new process instance using the "workflow web API"
